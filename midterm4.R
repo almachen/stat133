e2de41044f1b0303eb94598dcb29deed7f588e55
Alma Chen <almachen@berkeley.edu>
2015-04-30 15:27:48 -0700

    finished midterm4

--- midterm4.R
+++ midterm4.R
@@ -20,7 +20,19 @@ set.seed(123456)
 # We've set the default inputs to k=2 and B=100
 
 dice_sum <- function(k=2, B=100){
+	dsum <- c()
+	rollk <- c() 
+	for (i in 1:B){
+		for (j in 1:k) { # finds sum of multple dice rolls 
+			
+			each_roll <- sample(1:6, 1, replace = TRUE)
+			rollk <- c(rollk, each_roll)
+			sum_rolls <- sum(rollk)
+		}
+		dsum <- c(dsum, sum_rolls) 
+	} 
 
+return (dsum[length(dsum)]) 
 }
 
 #### String manipulation
@@ -29,20 +41,35 @@ phrases <- c("dog", "doggy", "den", "good boy", "Really?", "How much?", "Only $8
 
 # Create a vector [text1] that lists the elements in phrases 
 # where the SECOND TO LAST character is "o" (lower case o).
-#text1 <- <your code here>
+
+
+sub_phrases = c() 
+for (i in 1:length(phrases)){
+	sub_phrases <- c(sub_phrases, substring(phrases[i], 1, nchar(phrases[i])-1))
+}
+
+indices1 <- grep("o\\>", sub_phrases)
+text1 <- phrases[indices1]
 
 # Create a vector [text2] that lists the elements in phrases that
 # START with the letter "d"
-#text2 <- <your code here>
+first_letter = c() 
+for (i in 1:length(phrases)){
+	first_letter <- c(first_letter, substring(phrases[i], 1, 1))
+}
+indices2<- grep("d", first_letter) 
+text2 <- phrases[indices2]
 
-# Create a variable [no.punct] that equals the number of phrases with a punctuation mark in it.
-# no.punct <- <your code here>
+# Create a variable [no.punct] that equals the number of phrases
+#with a punctuation mark in it.
+no.punct <- length(grep("[[:punct:]]", phrases))
 
 # Create a vector [even] that is of length 1000 and has the entries
 # "even2", "even4", ...
 # with no separation between the word and the letter
+even_vec <- c(1:1000)*2
 
-#even <- <your code here>
+even <- paste("even", even_vec, sep='')
 
 
 # Start with [hotelCal] which is a character string, create 
@@ -53,7 +80,10 @@ phrases <- c("dog", "doggy", "den", "good boy", "Really?", "How much?", "Only $8
 
 hotelCal <- "On a dark desert highway, cool wind in my hair. Warm smell of colitas, rising up through the air. Up ahead in the distance, I saw a shimmering light. My head grew heavy and my sight grew dim I had to stop for the night.  There she stood in the doorway; I heard the mission bell.  And I was thinking to myself: 'This could be heaven or this could be hell'. Then she lit up a candle and she showed me the way."
 
-# hotelCal.split <- <your code here> 
+hotelCal.split <- unlist(strsplit(hotelCal, " "))
+hotelCal.split <- hotelCal.split[hotelCal.split != ""] # remove empty string
+hotelCal.lower <- tolower(hotelCal.split)
+hotelCal.nopunct <- gsub("[[:punct:]]", "", hotelCal.lower)
 
 
 # Write a function called updateDate. Your function should take the following
@@ -67,14 +97,19 @@ hotelCal <- "On a dark desert highway, cool wind in my hair. Warm smell of colit
 #     '2015'. This vector should only contain the elements whose date has been
 #     updated. For example updateDate(c('May, 2008', 'June, 2011'), '2008') should
 #     return 'May, 2015'.
+#dates_test <- c('May, 2008', 'June, 2011') 
 updateDate <- function(dates, old.yr) {
-
+old_indices <- grep(old.yr, dates)
+  updated.dates <- gsub(old.yr, '2015', dates[old_indices])
+  return(updated.dates)
 }
 
 
 # Write a function called [abbreviate] that takes in a vector of strings and returns
 # a vector of the same length with only the first [k] characters from the orignal vector entries.
-
+#abbre_test <- c("sdlfh", "alwehr", "wlekrh", "awel;kr")
 abbreviate <- function(vector, k){
+	abbre <- substring(vector, 1, k)
+	return (abbre)
 
 }
